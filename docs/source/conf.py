10bc39df327ca1f925c41a4b8424f1f27ff41a77
loevliedenny@gmail.com <59749099+loevlie@users.noreply.github.com>
2022-10-31 14:16:11 -0400

    Fixing sizes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(os.path.abspath('../..'))
+# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0,os.path.abspath('../..'))
 
 
 project = 'CANELa_Loevlie'
